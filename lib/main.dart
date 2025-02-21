7e974cb7cad503cb0553e86ab164971ef077f86c
Mahmud1729 <2001037@iot.bdu.ac.bd>
2025-02-21 18:12:54 +0600

    adding assets into project

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,22 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(appBar: AppBar(title: Text('Easy List'))),
+      home: Scaffold(
+        appBar: AppBar(title: Text('Easy List')),
+        body: Card(
+          child: Column(
+            children: <Widget>[
+              Image.asset('assets/food.jpg'),
+              Text('food paradise'),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
